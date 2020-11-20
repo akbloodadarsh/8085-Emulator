6e1a5fa7088daf169e8b9de4e2a9f4011e878e39
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-11-20 14:04:09 +0530

    beautify the code

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -1,72 +1,87 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int flag_on_off_8bit_ADD(int &val1)
 {
-	if((val1&15)+(val1&15)>15)AC=1;
-	int val=val1+val1;
-	if(val>255)
+	if ((val1 & 15) + (val1 & 15) > 15)
+		AC = 1;
+	int val = val1 + val1;
+	if (val > 255)
 	{
-		CF=1;
-		val-=256;
+		CF = 1;
+		val -= 256;
 	}
-	if(!val)ZF=1;
+	if (!val)
+		ZF = 1;
 	int parity_checker_bit = 1;
 	int parity_counter = 0;
-	for(int i=0;i<8;++i)
+	for (int i = 0; i < 8; ++i)
 	{
-		if(parity_checker_bit&val)++parity_counter;
-		parity_checker_bit<<=1;
+		if (parity_checker_bit & val)
+			++parity_counter;
+		parity_checker_bit <<= 1;
 	}
 	PF = 1;
-	if(parity_counter&1)PF = 0;
-	if(val<0)SF=1;
+	if (parity_counter & 1)
+		PF = 0;
+	if (val < 0)
+		SF = 1;
 	return val;
-}	
+}
 
-int flag_on_off_8bit_ADD(int &val1,int &val2)
+int flag_on_off_8bit_ADD(int &val1, int &val2)
 {
-	if((val1&15)+(val2&15)>15)AC=1;
-	int val=val1+val2;
-	if(val>255)
+	if ((val1 & 15) + (val2 & 15) > 15)
+		AC = 1;
+	int val = val1 + val2;
+	if (val > 255)
 	{
-		CF=1;
-		val-=256;
+		CF = 1;
+		val -= 256;
 	}
-	if(!val)ZF=1;
+	if (!val)
+		ZF = 1;
 	int parity_checker_bit = 1;
 	int parity_counter = 0;
-	for(int i=0;i<8;++i)
+	for (int i = 0; i < 8; ++i)
 	{
-		if(parity_checker_bit&val)++parity_counter;
-		parity_checker_bit<<=1;
+		if (parity_checker_bit & val)
+			++parity_counter;
+		parity_checker_bit <<= 1;
 	}
 	PF = 1;
-	if(parity_counter&1)PF = 0;
-	if(val<0)SF=1;
+	if (parity_counter & 1)
+		PF = 0;
+	if (val < 0)
+		SF = 1;
 	return val;
-}	
+}
 
-int flag_on_off_8bit_SUB(int &val1,int &val2)
+int flag_on_off_8bit_SUB(int &val1, int &val2)
 {
-	if(val1<val2)SF=CF=1;
-	int tmp_val2 = val2^255;
+	if (val1 < val2)
+		SF = CF = 1;
+	int tmp_val2 = val2 ^ 255;
 	++tmp_val2;
-	if((val1&15)+(tmp_val2&15)>15)AC=1;
-	int val=val1-val2;
+	if ((val1 & 15) + (tmp_val2 & 15) > 15)
+		AC = 1;
+	int val = val1 - val2;
 	int parity_checker_bit = 1;
 	int parity_counter = 0;
-	for(int i=0;i<8;++i)
+	for (int i = 0; i < 8; ++i)
 	{
-		if(parity_checker_bit&val)++parity_counter;
-		parity_checker_bit<<=1;
+		if (parity_checker_bit & val)
+			++parity_counter;
+		parity_checker_bit <<= 1;
 	}
 	PF = 1;
-	if(parity_counter&1)PF = 0;
-	if(!val)ZF=1;
-	if(val<0)
+	if (parity_counter & 1)
+		PF = 0;
+	if (!val)
+		ZF = 1;
+	if (val < 0)
 	{
-		val*=-1;
+		val *= -1;
 	}
 	return val;
 }
@@ -80,119 +95,119 @@ int flag_on_off_8bit_SUB(int &val1)
 
 void ADD(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
-	switch(user_instruction[4])
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
+	switch (user_instruction[4])
 	{
-		case 'A':
-				A=flag_on_off_8bit_ADD(A);
-				break;
-		case 'B':
-				A=flag_on_off_8bit_ADD(A,B);
-				break;
-		case 'C':
-				A=flag_on_off_8bit_ADD(A,C);
-				 break;
-		case 'D':
-				A=flag_on_off_8bit_ADD(A,D);
-				break;
-		case 'E':
-				A=flag_on_off_8bit_ADD(A,E);
-				break;
-		case 'H':
-				A=flag_on_off_8bit_ADD(A,H);
-				break;
-		case 'L':
-				A=flag_on_off_8bit_ADD(A,L);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'A':
+		A = flag_on_off_8bit_ADD(A);
+		break;
+	case 'B':
+		A = flag_on_off_8bit_ADD(A, B);
+		break;
+	case 'C':
+		A = flag_on_off_8bit_ADD(A, C);
+		break;
+	case 'D':
+		A = flag_on_off_8bit_ADD(A, D);
+		break;
+	case 'E':
+		A = flag_on_off_8bit_ADD(A, E);
+		break;
+	case 'H':
+		A = flag_on_off_8bit_ADD(A, H);
+		break;
+	case 'L':
+		A = flag_on_off_8bit_ADD(A, L);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 }
 
 void ADI(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
 	string hex_number = "";
 	hex_number.push_back(user_instruction[4]);
 	hex_number.push_back(user_instruction[5]);
 	int tmp = hex_to_dec(hex_number);
-	A=flag_on_off_8bit_ADD(A,tmp);
+	A = flag_on_off_8bit_ADD(A, tmp);
 }
 
 void SUI(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
 	string hex_number = "";
 	hex_number.push_back(user_instruction[4]);
 	hex_number.push_back(user_instruction[5]);
 	int tmp = hex_to_dec(hex_number);
-	A=flag_on_off_8bit_SUB(A,tmp);
+	A = flag_on_off_8bit_SUB(A, tmp);
 }
 
 void SUB(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
-	switch(user_instruction[4])
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
+	switch (user_instruction[4])
 	{
-		case 'A':
-				A=flag_on_off_8bit_SUB(A);
-				break;
-		case 'B':
-				A=flag_on_off_8bit_SUB(A,B);
-				break;
-		case 'C':
-				A=flag_on_off_8bit_SUB(A,C);
-				 break;
-		case 'D':
-				A=flag_on_off_8bit_SUB(A,D);
-				break;
-		case 'E':
-				A=flag_on_off_8bit_SUB(A,E);
-				break;
-		case 'H':
-				A=flag_on_off_8bit_SUB(A,H);
-				break;
-		case 'L':
-				A=flag_on_off_8bit_SUB(A,L);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'A':
+		A = flag_on_off_8bit_SUB(A);
+		break;
+	case 'B':
+		A = flag_on_off_8bit_SUB(A, B);
+		break;
+	case 'C':
+		A = flag_on_off_8bit_SUB(A, C);
+		break;
+	case 'D':
+		A = flag_on_off_8bit_SUB(A, D);
+		break;
+	case 'E':
+		A = flag_on_off_8bit_SUB(A, E);
+		break;
+	case 'H':
+		A = flag_on_off_8bit_SUB(A, H);
+		break;
+	case 'L':
+		A = flag_on_off_8bit_SUB(A, L);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 }
 
 void DCR(string &user_instruction)
 {
 	bool tmp_CF = CF;
-	SF=0,ZF=0,AC=0,PF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0;
 	int tmp = 1;
-	switch(user_instruction[4])
+	switch (user_instruction[4])
 	{
-		case 'A':
-				A=flag_on_off_8bit_SUB(A,tmp);
-				break;
-		case 'B':
-				B=flag_on_off_8bit_SUB(B,tmp);
-				break;
-		case 'C':
-				C=flag_on_off_8bit_SUB(C,tmp);
-				 break;
-		case 'D':
-				D=flag_on_off_8bit_SUB(D,tmp);
-				break;
-		case 'E':
-				E=flag_on_off_8bit_SUB(E,tmp);
-				break;
-		case 'H':
-				H=flag_on_off_8bit_SUB(H,tmp);
-				break;
-		case 'L':
-				L=flag_on_off_8bit_SUB(L,tmp);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'A':
+		A = flag_on_off_8bit_SUB(A, tmp);
+		break;
+	case 'B':
+		B = flag_on_off_8bit_SUB(B, tmp);
+		break;
+	case 'C':
+		C = flag_on_off_8bit_SUB(C, tmp);
+		break;
+	case 'D':
+		D = flag_on_off_8bit_SUB(D, tmp);
+		break;
+	case 'E':
+		E = flag_on_off_8bit_SUB(E, tmp);
+		break;
+	case 'H':
+		H = flag_on_off_8bit_SUB(H, tmp);
+		break;
+	case 'L':
+		L = flag_on_off_8bit_SUB(L, tmp);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 	CF = tmp_CF;
 }
@@ -200,162 +215,170 @@ void DCR(string &user_instruction)
 void INR(string &user_instruction)
 {
 	bool tmp_CF = CF;
-	SF=0,ZF=0,AC=0,PF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0;
 	int tmp = 1;
-	switch(user_instruction[4])
+	switch (user_instruction[4])
 	{
-		case 'A':
-				A=flag_on_off_8bit_ADD(A,tmp);
-				break;
-		case 'B':
-				B=flag_on_off_8bit_ADD(B,tmp);
-				break;
-		case 'C':
-				C=flag_on_off_8bit_ADD(C,tmp);
-				 break;
-		case 'D':
-				D=flag_on_off_8bit_ADD(D,tmp);
-				break;
-		case 'E':
-				E=flag_on_off_8bit_ADD(E,tmp);
-				break;
-		case 'H':
-				H=flag_on_off_8bit_ADD(H,tmp);
-				break;
-		case 'L':
-				L=flag_on_off_8bit_ADD(L,tmp);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'A':
+		A = flag_on_off_8bit_ADD(A, tmp);
+		break;
+	case 'B':
+		B = flag_on_off_8bit_ADD(B, tmp);
+		break;
+	case 'C':
+		C = flag_on_off_8bit_ADD(C, tmp);
+		break;
+	case 'D':
+		D = flag_on_off_8bit_ADD(D, tmp);
+		break;
+	case 'E':
+		E = flag_on_off_8bit_ADD(E, tmp);
+		break;
+	case 'H':
+		H = flag_on_off_8bit_ADD(H, tmp);
+		break;
+	case 'L':
+		L = flag_on_off_8bit_ADD(L, tmp);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 	CF = tmp_CF;
 }
 
 void INX(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
 	int temp;
-	switch(user_instruction[4])
+	switch (user_instruction[4])
 	{
-		case 'B':
-				temp = hex_to_dec(dec_to_hex(B)+dec_to_hex(C));
-				break;
-		case 'D':
-				temp = hex_to_dec(dec_to_hex(D)+dec_to_hex(E));
-				break;
-		case 'H':
-				temp = hex_to_dec(dec_to_hex(H)+dec_to_hex(L));
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'B':
+		temp = hex_to_dec(dec_to_hex(B) + dec_to_hex(C));
+		break;
+	case 'D':
+		temp = hex_to_dec(dec_to_hex(D) + dec_to_hex(E));
+		break;
+	case 'H':
+		temp = hex_to_dec(dec_to_hex(H) + dec_to_hex(L));
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 	++temp;
-	if(temp>hex_to_dec("FFFF"))temp-=hex_to_dec("FFFF");
+	if (temp > hex_to_dec("FFFF"))
+		temp -= hex_to_dec("FFFF");
 	string val = dec_to_hex16(temp);
-	string address1 = "",address2 = "";
-	for(int i=0;i<=1;++i)address1.push_back(val[i]);
-	for(int i=2;i<=3;++i)address2.push_back(val[i]);
-	switch(user_instruction[4])
+	string address1 = "", address2 = "";
+	for (int i = 0; i <= 1; ++i)
+		address1.push_back(val[i]);
+	for (int i = 2; i <= 3; ++i)
+		address2.push_back(val[i]);
+	switch (user_instruction[4])
 	{
-		case 'B':
-				C = hex_to_dec(address2);
-				B = hex_to_dec(address1);	
-				break;
-		case 'D':
-				E = hex_to_dec(address2);
-				D = hex_to_dec(address1);
-				break;
-		case 'H':
-				L = hex_to_dec(address2);
-				H = hex_to_dec(address1);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'B':
+		C = hex_to_dec(address2);
+		B = hex_to_dec(address1);
+		break;
+	case 'D':
+		E = hex_to_dec(address2);
+		D = hex_to_dec(address1);
+		break;
+	case 'H':
+		L = hex_to_dec(address2);
+		H = hex_to_dec(address1);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 }
 
 void DCX(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
 	int temp;
-	switch(user_instruction[4])
+	switch (user_instruction[4])
 	{
-		case 'B':
-				temp = hex_to_dec(dec_to_hex(B)+dec_to_hex(C));
-				break;
-		case 'D':
-				temp = hex_to_dec(dec_to_hex(D)+dec_to_hex(E));
-				break;
-		case 'H':
-				temp = hex_to_dec(dec_to_hex(H)+dec_to_hex(L));
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'B':
+		temp = hex_to_dec(dec_to_hex(B) + dec_to_hex(C));
+		break;
+	case 'D':
+		temp = hex_to_dec(dec_to_hex(D) + dec_to_hex(E));
+		break;
+	case 'H':
+		temp = hex_to_dec(dec_to_hex(H) + dec_to_hex(L));
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 	--temp;
-	cout<<temp<<"\n";
-	if(temp<0)temp*=-1;
-	cout<<temp<<"\n";
+	cout << temp << "\n";
+	if (temp < 0)
+		temp *= -1;
+	cout << temp << "\n";
 	string val = dec_to_hex16(temp);
-	string address1 = "",address2 = "";
-	for(int i=0;i<=1;++i)address1.push_back(val[i]);
-	for(int i=2;i<=3;++i)address2.push_back(val[i]);
-	switch(user_instruction[4])
+	string address1 = "", address2 = "";
+	for (int i = 0; i <= 1; ++i)
+		address1.push_back(val[i]);
+	for (int i = 2; i <= 3; ++i)
+		address2.push_back(val[i]);
+	switch (user_instruction[4])
 	{
-		case 'B':
-				C = hex_to_dec(address2);
-				B = hex_to_dec(address1);	
-				break;
-		case 'D':
-				E = hex_to_dec(address2);
-				D = hex_to_dec(address1);
-				break;
-		case 'H':
-				L = hex_to_dec(address2);
-				H = hex_to_dec(address1);
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'B':
+		C = hex_to_dec(address2);
+		B = hex_to_dec(address1);
+		break;
+	case 'D':
+		E = hex_to_dec(address2);
+		D = hex_to_dec(address1);
+		break;
+	case 'H':
+		L = hex_to_dec(address2);
+		H = hex_to_dec(address1);
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
 }
 
 void DAD(string &user_instruction)
 {
-	SF=0,ZF=0,AC=0,PF=0,CF=0;
-	int val1 = hex_to_dec(dec_to_hex(H)+dec_to_hex(L)),val2;
-	switch(user_instruction[4])
+	SF = 0, ZF = 0, AC = 0, PF = 0, CF = 0;
+	int val1 = hex_to_dec(dec_to_hex(H) + dec_to_hex(L)), val2;
+	switch (user_instruction[4])
 	{
-		case 'B':
-				val2 = hex_to_dec(dec_to_hex(B)+dec_to_hex(C));
-				break;
-		case 'D':
-				val2 = hex_to_dec(dec_to_hex(D)+dec_to_hex(E));
-				break;
-		case 'H':
-				val2 = val1;
-				break;
-		default: 
-				error = 1;
-				cout<<"You found a bug!\n";
+	case 'B':
+		val2 = hex_to_dec(dec_to_hex(B) + dec_to_hex(C));
+		break;
+	case 'D':
+		val2 = hex_to_dec(dec_to_hex(D) + dec_to_hex(E));
+		break;
+	case 'H':
+		val2 = val1;
+		break;
+	default:
+		error = 1;
+		cout << "You found a bug!\n";
 	}
-	val1+=val2;
-	if(val1>hex_to_dec("FFFF"))
+	val1 += val2;
+	if (val1 > hex_to_dec("FFFF"))
 	{
-		CF=1;
-		val1-=hex_to_dec("FFFF")+1;
+		CF = 1;
+		val1 -= hex_to_dec("FFFF") + 1;
 	}
-	cout<<val1<<"\n";
+	cout << val1 << "\n";
 	string val = dec_to_hex16(val1);
-	cout<<val<<"\n";
-	string address1 = "",address2 = "";
-	for(int i=0;i<=1;++i)address1.push_back(val[i]);
-	for(int i=2;i<=3;++i)address2.push_back(val[i]);
-	cout<<address1<<" "<<address2<<"\n";
+	cout << val << "\n";
+	string address1 = "", address2 = "";
+	for (int i = 0; i <= 1; ++i)
+		address1.push_back(val[i]);
+	for (int i = 2; i <= 3; ++i)
+		address2.push_back(val[i]);
+	cout << address1 << " " << address2 << "\n";
 	L = hex_to_dec(address2);
 	H = hex_to_dec(address1);
 }
--- extras.cpp
+++ extras.cpp
@@ -1,45 +1,12 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void help()
 {
-	cout<<"Arithmetic Instructions\n
-		ADD Ex:- ADD B\n
-		ADI Ex:- ADI 45H\n
-		SUI Ex:- SUI 24H\n
-		SUB Ex:- SUB D\n
-		DCR Ex:- DCR A\n
-		INR Ex:- INR B\n
-		INX Ex:- INX B\n
-		DCX Ex:- DCX B\n
-		DAD Ex:- DAD B\n\n
-		Logical Instructions\n
-		CMA Ex:- CMA\n
-		CMP Ex:- CMP B\n\n
-		Branching Instructions\n
-		JMP Ex:- JMP 4000H\n
-		JC Ex:- JC 4000H\n
-		JNC Ex:- JNC 5000H\n
-		JZ Ex:- JZ 8000H\n\n
-		Load and Store Instructions\n
-		MOV Ex:- MOV B, A\n
-		MVI Ex:- MVI A, 54H\n
-		LDA Ex:- LDA 4000H\n
-		STA Ex:- STA 4000H\n
-		SHLD Ex:- 4000H\n
-		LHLD Ex:- LHLD 5000H\n
-		XCHG Ex:- XCHG\n
-		LXI Ex:- LXI H, 4000H\n
-		STAX Ex:- STAX B\n\n
-		";
+	cout << "Arithmetic Instructions\nADD Ex:- ADD B\nADI Ex:- ADI 45H\nSUI Ex:- SUI 24H\nSUB Ex:- SUB D\nDCR Ex:- DCR A\nINR Ex:- INR B\nINX Ex:- INX B\n		DCX Ex:- DCX B\n		DAD Ex:- DAD B\n\nLogical Instructions\n		CMA Ex:- CMA\n		CMP Ex:- CMP B\n\n		Branching Instructions\n		JMP Ex:- JMP 4000H\n		JC Ex:- JC 4000H\n		JNC Ex:- JNC 5000H\n		JZ Ex:- JZ 8000H\n\n		Load and Store Instructions\n		MOV Ex:- MOV B, A\n		MVI Ex:- MVI A, 54H\n		LDA Ex:- LDA 4000H\n		STA Ex:- STA 4000H\n		SHLD Ex:- 4000H\n		LHLD Ex:- LHLD 5000H\n		XCHG Ex:- XCHG\nLXI Ex:- LXI H, 4000H\nSTAX Ex:- STAX B\n\n";
 }
 
 void help_debug()
 {
-	SET Ex:- SET 2500H, 05H\n
-	break or b Ex:- break 2500H or b 2500H\n
-	step or s Ex:- step or s\n
-	print or p Ex:- print A or p B or print 2500H or p 5000H\n
-	quit or q Ex:- quit or q\n
-	help Ex:- help\n\n";
+	cout << "SET Ex:- SET 2500H, 05H\n	break or b Ex:- break 2500H or b 2500H\n	step or s Ex:- step or s\n	print or p Ex:- print A or p B or print 2500H or p 5000H\n	quit or q Ex:- quit or q\n	help Ex:- help\n\n";
 }
